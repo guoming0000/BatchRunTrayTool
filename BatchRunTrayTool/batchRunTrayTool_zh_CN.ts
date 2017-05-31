b6eb4c491ed727a5803dbfc8214065e8cb68c3ea
Guo Ming <i@brightguo.com>
2017-05-31 18:19:02 +0800

    add iss config file for my self to build setup

--- BatchRunTrayTool/batchRunTrayTool_zh_CN.ts
+++ BatchRunTrayTool/batchRunTrayTool_zh_CN.ts
@@ -14,27 +14,27 @@
         <translation type="unfinished">文件丢失 : %1</translation>
     </message>
     <message>
-        <location filename="MainDialog.cpp" line="98"/>
+        <location filename="MainDialog.cpp" line="107"/>
         <source>About</source>
         <translation type="unfinished">关于</translation>
     </message>
     <message>
-        <location filename="MainDialog.cpp" line="101"/>
+        <location filename="MainDialog.cpp" line="110"/>
         <source>Project Page</source>
         <translation type="unfinished">项目地址</translation>
     </message>
     <message>
-        <location filename="MainDialog.cpp" line="106"/>
+        <location filename="MainDialog.cpp" line="115"/>
         <source>Feedback</source>
         <translation type="unfinished">问题反馈</translation>
     </message>
     <message>
-        <location filename="MainDialog.cpp" line="111"/>
+        <location filename="MainDialog.cpp" line="120"/>
         <source>Usage</source>
         <translation type="unfinished">使用方法</translation>
     </message>
     <message>
-        <location filename="MainDialog.cpp" line="116"/>
+        <location filename="MainDialog.cpp" line="125"/>
         <source>Exit</source>
         <translation type="unfinished">退出</translation>
     </message>
@@ -42,12 +42,12 @@
 <context>
     <name>QObject</name>
     <message>
-        <location filename="main.cpp" line="15"/>
+        <location filename="main.cpp" line="20"/>
         <source>Systray</source>
         <translation type="unfinished">系统托盘</translation>
     </message>
     <message>
-        <location filename="main.cpp" line="16"/>
+        <location filename="main.cpp" line="21"/>
         <source>I couldn&apos;t detect any system tray on this system.</source>
         <translation type="unfinished">该系统可能不支持托盘功能。</translation>
     </message>
